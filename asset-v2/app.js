406cd2f9e227439326b62bb37ee367d0708717c4
Joalor64 <rhgarcon@gmail.com>
2022-11-22 18:53:17 -0500

    testing and stuff

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -1,35 +1,35 @@
 /* 0.5.4 2021-04-23 21:06:13 */
 var app = new function() {
-    this.name = "Little Miss", this.version = "2", this.date = "2012", this.folder = "asset-v2/", this.looptime = 4800, this.bpm = 100, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#EB4B4B", this.col1 = "#C32D2D", this.col2 = "#9B2323", this.col3 = "#691E1E", this.col4 = "#371919", this.animearray = [{
-        name: "beat1_boom",
-        color: "828282",
-        uniqsnd: !1
-    }, {
-        name: "beat2_kashi",
-        color: "828282",
-        uniqsnd: !1
-    }, {
-        name: "beat3_paomeu",
+    this.name = "Khram", this.version = "2", this.date = "2022", this.folder = "asset-v2/", this.looptime = 6500, this.bpm = 110, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#EB4B4B", this.col1 = "#C32D2D", this.col2 = "#9B2323", this.col3 = "#691E1E", this.col4 = "#371919", this.animearray = [{
+        name: "1_pinat",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "beat4_ptttpeu",
-        color: "828282",
-        uniqsnd: !1
-    }, {
-        name: "beat5_slupttt",
+        name: "2_baraban",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "effet1_poulll",
-        color: "828282",
-        uniqsnd: !1
-    }, {
-        name: "effet2_tucati",
+        name: "3_bongi",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "effet3_tuilopta",
+        name: "4_obod",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "5_blokirovat",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "6_basovyy",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "7_vibroshlepok",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "8_sheyker",
         color: "828282",
         uniqsnd: !1
     }, {
@@ -39,27 +39,27 @@ var app = new function() {
     }, {
         name: "effet5_tumttt",
         color: "828282",
-        uniqsnd: !1
+        uniqsnd: !0
     }, {
-        name: "melo1_nananana",
+        name: "11_zashchitnik",
         color: "828282",
-        uniqsnd: !1
+        uniqsnd: !0
     }, {
-        name: "melo2_pelulu",
+        name: "12_pianino",
         color: "828282",
-        uniqsnd: !1
+        uniqsnd: !0
     }, {
-        name: "melo3_siffle",
+        name: "13_piloobraznyy",
         color: "828282",
-        uniqsnd: !1
+        uniqsnd: !0
     }, {
-        name: "melo4_tatouti",
+        name: "14_skripka",
         color: "828282",
-        uniqsnd: !1
+        uniqsnd: !0
     }, {
-        name: "melo5_tvutvutvu",
+        name: "15_truba",
         color: "828282",
-        uniqsnd: !1
+        uniqsnd: !0
     }, {
         name: "coeur1_oaaah",
         color: "828282",
