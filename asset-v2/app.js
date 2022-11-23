52c149c8b8eee47d31cbf31385bec5d98eb3f891
Joalor64 <rhgarcon@gmail.com>
2022-11-23 09:53:31 -0500

    Update app.js

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -1,6 +1,6 @@
 /* 0.5.4 2021-04-23 21:06:13 */
 var app = new function() {
-    this.name = "Khram", this.version = "2", this.date = "2022", this.folder = "asset-v2/", this.looptime = 6572, this.bpm = 110, this.totalframe = 232, this.nbpolo = 13, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#EB4B4B", this.col1 = "#C32D2D", this.col2 = "#9B2323", this.col3 = "#691E1E", this.col4 = "#371919", this.animearray = [{
+    this.name = "Khram", this.version = "2", this.date = "2022", this.folder = "asset-v2/", this.looptime = 6572, this.bpm = 110, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#EB4B4B", this.col1 = "#C32D2D", this.col2 = "#9B2323", this.col3 = "#691E1E", this.col4 = "#371919", this.animearray = [{
         name: "1_pinat",
         color: "828282",
         uniqsnd: !0
@@ -83,19 +83,19 @@ var app = new function() {
     }], this.bonusarray = [{
         name: "Satisfied",
         src: "v2-b1-satisfy-hb.mp4",
-        code: "1,4,6,9,12",
+        code: "1,2,5,11,12",
         sound: "bonus-satisfy",
         aspire: "aspire"
     }, {
         name: "Little Miss",
         src: "v2-b2-miss-hb.mp4",
-        code: "5,6,7,13,15",
+        code: "2,4,6,7,15",
         sound: "bonus-miss",
         aspire: "aspire"
     }, {
         name: "Why this world",
         src: "v2-b3-world-hb.mp4",
-        code: "6,8,9,14,18",
+        code: "3,5,8,13,14",
         sound: "bonus-world",
         aspire: "aspire"
     }];
